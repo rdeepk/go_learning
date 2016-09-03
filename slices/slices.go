f3b81807698cb0142a8501fb55a382e61fcc294b
Ramandeep Kaur <rdeepk@gmail.com>
2016-09-02 19:26:14 -0700

    Go Slices

---
+++ slices/slices.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := make([]int, 5)
+	printSlice("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+
+	c := b[:2]
+	printSlice("c", c)
+
+	d := c[2:5]
+	printSlice("d", d)
+}
+
+func printSlice(s string, num []int) {
+	fmt.Println("%s len=%d cap=%d %v",s, len(num), cap(num), num)
+}
+
