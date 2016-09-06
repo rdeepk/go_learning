b639d447f1fba49d1836657d889c9c3cfade52ef
Ramandeep Kaur <rdeepk@gmail.com>
2016-09-06 00:57:16 -0700

    Errors exercise from Golang tour

---
+++ errors/errors.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %f", e)
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x<0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z := float64(1)
+        for i := 0; i < 1000; i++ {
+            z = z - (z * z - x)/(2 * z)
+        }
+    return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
