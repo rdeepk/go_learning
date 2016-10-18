d6b78998dd7699f16b70427b7c46d7dbe3fe27ec
Ramandeep Kaur <rdeepk@gmail.com>
2016-10-17 18:06:14 -0700

    This is first example of solution with use of sync package

--- concurrency/mutex/mutex.go
+++ concurrency/mutex/mutex.go
@@ -5,15 +5,19 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
 func main() {
 	runtime.GOMAXPROCS(4)
+	mutex := new(sync.Mutex)
 	for i := 1; i < 10; i++ {
 		for j := 1; j < 10; j++ {
+			mutex.Lock()
 			go func() {
 				//the access of i and j in goroutines is wrong but this use is //just to arise the case of cuncurrent memory access
 				fmt.Printf("%d + %d = %d\n", i, j, i+j)
+				mutex.Unlock()
 			}()
 		}
 	}
