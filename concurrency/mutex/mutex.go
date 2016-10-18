81ee61c76e0bdeaaa9ff73318dc737409aeed415
Ramandeep Kaur <rdeepk@gmail.com>
2016-10-17 19:32:45 -0700

    Logging output in a file

--- concurrency/mutex/mutex.go
+++ concurrency/mutex/mutex.go
@@ -4,20 +4,41 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
-	"sync"
+	"time"
 )
 
 func main() {
 	runtime.GOMAXPROCS(4)
-	mutex := new(sync.Mutex)
+	f, _ := os.Create("./log.txt")
+	f.Close()
+
+	logCh := make(chan string, 50)
+	go func() {
+		for {
+			msg, ok := <-logCh
+			if ok {
+				f, _ := os.OpenFile("./log.txt", os.O_RDWR|os.O_APPEND, os.ModeAppend)
+				logtime := time.Now().Format(time.RFC3339)
+				f.WriteString(logtime + " " + msg)
+				f.Close()
+			} else {
+				break
+			}
+		}
+	}()
+
+	mutex := make(chan bool, 1)
 	for i := 1; i < 10; i++ {
 		for j := 1; j < 10; j++ {
-			mutex.Lock()
+			mutex <- true
 			go func() {
 				//the access of i and j in goroutines is wrong but this use is //just to arise the case of cuncurrent memory access
-				fmt.Printf("%d + %d = %d\n", i, j, i+j)
-				mutex.Unlock()
+				msg := fmt.Sprintf("%d + %d = %d\n", i, j, i+j)
+				logCh <- msg
+				fmt.Print(msg)
+				<-mutex
 			}()
 		}
 	}
