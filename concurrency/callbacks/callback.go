305806843267ef42ba7555b5ae681b35144cc7e5
Ramandeep Kaur <rdeepk@gmail.com>
2016-10-18 18:58:14 -0700

    Simulating callbacks

---
+++ concurrency/callbacks/callback.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+type PurchaseOrder struct {
+	number int
+	value  float64
+}
+
+func main() {
+	po := new(PurchaseOrder)
+	po.value = 56.67
+	ch := make(chan *PurchaseOrder)
+
+	go SavePO(po, ch)
+	newPO := <-ch
+
+	fmt.Printf("New Purchase Order: %v", newPO)
+
+}
+
+func SavePO(po *PurchaseOrder, callback chan *PurchaseOrder) {
+	po.number = 1234
+	callback <- po
+}
